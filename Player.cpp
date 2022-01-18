1950d2b6b0bfa52d841da084637bae89964fee10
Peter Irshad <pete.t3141@gmail.com>
2022-01-18 03:52:38 -0500

    Refactored Game Pieces
    - Converted separate lists of different game piece types into a list containing list of game piece types (specific subscripts correspond to specific game piece types).
    - This change resulted in removing duplicate code.

--- Player.cpp
+++ Player.cpp
@@ -2,93 +2,37 @@
 
 Player::Player(Board* board) {
 	_board = board;
-	_gamePieces = new std::vector<GamePiece*>();
-	_queenBee = new GamePiece(board, this, QUEEN_BEE);
-	_beetles = new std::vector<GamePiece*>();
-	_grasshoppers = new std::vector<GamePiece*>();
-	_spiders = new std::vector<GamePiece*>();
-	_soldierAnts = new std::vector<GamePiece*>();
-	for (int i = 0; i < BEETLE_AMOUNT; i++)
+	_gamePieces = new std::vector<std::vector<GamePiece*>*>();
+	for (int i = 0; i < AMOUNT_OF_PIECE_TYPES; i++)
 	{
-		_beetles->push_back(new GamePiece(board, this, BEETLE));
-	}
-	for (int i = 0; i < GRASSHOPPER_AMOUNT; i++)
-	{
-		_grasshoppers->push_back(new GamePiece(board, this, GRASSHOPPER));
-	}
-	for (int i = 0; i < SPIDER_AMOUNT; i++)
-	{
-		_spiders->push_back(new GamePiece(board, this, SPIDER));
-	}
-	for (int i = 0; i < SOLDIER_ANT_AMOUNT; i++)
-	{
-		_soldierAnts->push_back(new GamePiece(board, this, SOLDIER_ANT));
+		_gamePieces->push_back(new std::vector<GamePiece*>());
+		for (int j = 0; j < PIECE_AMOUNTS[i]; j++)
+		{
+			_gamePieces->at(i)->push_back(new GamePiece(board, this, static_cast<GamePieceType>(i)));
+		}
 	}
 }
 
-std::vector<GamePiece*>* Player::getGamePieces() {
+std::vector<std::vector<GamePiece*>*>* Player::getGamePieces() {
 	return _gamePieces;
 };
 
 bool Player::placeGamePiece(Coordinate* coordinate, GamePieceType gamePieceType) {
 	GamePiece* selectedGamePiece = nullptr;
 
-	//TO-DO: Duplicate code. Refactor various arrays into an array of arrays later on.
-	if (gamePieceType == QUEEN_BEE)
+	std::vector<GamePiece*>* currentGamePieceList = _gamePieces->at(static_cast<GamePieceType>(gamePieceType));
+
+	for (int i = 0; i < currentGamePieceList->size(); i++)
 	{
-		if (_queenBee->getHexNode() == nullptr) {
-			selectedGamePiece = _queenBee;
+		if (currentGamePieceList->at(i)->getHexNode() == nullptr)
+		{
+			selectedGamePiece = currentGamePieceList->at(i);
+			break;
 		}
-		else {
+		else if (i == PIECE_AMOUNTS[static_cast<GamePieceType>(gamePieceType)] - 1)
+		{
+			std::cout << "The type of piece you want to place are all on the board!" << std::endl;
 			return false;
-		}
-	}
-	else if (gamePieceType == BEETLE)
-	{
-		for (int i = 0; i < BEETLE_AMOUNT; i++) {
-			if (_beetles->at(i)->getHexNode() == nullptr) {
-				selectedGamePiece = _beetles->at(i);
-				break;
-			}
-			else if (i == BEETLE_AMOUNT - 1) {
-				return false;
-			}
-		}
-	}
-	else if (gamePieceType == GRASSHOPPER)
-	{
-		for (int i = 0; i < GRASSHOPPER_AMOUNT; i++) {
-			if (_grasshoppers->at(i)->getHexNode() == nullptr) {
-				selectedGamePiece = _grasshoppers->at(i);
-				break;
-			}
-			else if (i == GRASSHOPPER_AMOUNT - 1) {
-				return false;
-			}
-		}
-	}
-	else if (gamePieceType == SPIDER)
-	{
-		for (int i = 0; i < SPIDER_AMOUNT; i++) {
-			if (_spiders->at(i)->getHexNode() == nullptr) {
-				selectedGamePiece = _spiders->at(i);
-				break;
-			}
-			else if (i == SPIDER_AMOUNT - 1) {
-				return false;
-			}
-		}
-	}
-	else //Soldier Ant
-	{
-		for (int i = 0; i < SOLDIER_ANT_AMOUNT; i++) {
-			if (_soldierAnts->at(i)->getHexNode() == nullptr) {
-				selectedGamePiece = _soldierAnts->at(i);
-				break;
-			}
-			else if (i == SOLDIER_ANT_AMOUNT - 1) {
-				return false;
-			}
 		}
 	}
 
--- Player.h
+++ Player.h
@@ -6,27 +6,19 @@
 #include "Board.h"
 #include "GamePiece.h"
 
-const int QUEEN_BEE_AMOUNT = 1;
-const int BEETLE_AMOUNT = 2;
-const int GRASSHOPPER_AMOUNT = 3;
-const int SPIDER_AMOUNT = 2;
-const int SOLDIER_ANT_AMOUNT = 3;
+const int AMOUNT_OF_PIECE_TYPES = 5;
+//In order: Queen Bee, Beetle, Grasshopper, Spider, Soldier Ant
+const int PIECE_AMOUNTS[AMOUNT_OF_PIECE_TYPES] = {1, 2, 3, 2, 3};
 
 class Player
 {
 private:
 	Board* _board;
-	std::vector<GamePiece*>* _gamePieces;
-
-	GamePiece* _queenBee;
-	std::vector<GamePiece*>* _beetles;
-	std::vector<GamePiece*>* _grasshoppers;
-	std::vector<GamePiece*>* _spiders;
-	std::vector<GamePiece*>* _soldierAnts;
+	std::vector<std::vector<GamePiece*>*>* _gamePieces;
 
 public:
 	Player(Board* board);
-	std::vector<GamePiece*>* getGamePieces();
+	std::vector<std::vector<GamePiece*>*>* getGamePieces();
 	bool placeGamePiece(Coordinate* coordinate, GamePieceType gamePieceType);
 };
 
